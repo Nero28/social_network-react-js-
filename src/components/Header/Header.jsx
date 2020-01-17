24d0dd8c5a86c649d30cec7715889b203501237a
nero <shackiy.s@yandex.ru>
2020-01-17 16:59:52 +0300

    folder structure,new components, improve css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className= {s.header}>
+        <img src='https://placeit-assets2.s3-accelerate.amazonaws.com/custom-pages/clothing-logo-maker/fun-logo-generator-for-a-skating-focused-apparel-brand-2592f-1024x1024.png'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+import Post from './Post/Post';
+
+const MyPosts = () => {
+  return (
+    <div> my posts
+     <div className={s.posts}>
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  )
+}
+
+export default MyPosts;
