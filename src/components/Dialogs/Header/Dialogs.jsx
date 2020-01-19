db64c8907b2c75e7e7a2e9b1a391b9d0ae96ffe6
nero <shackiy.s@yandex.ru>
2020-01-19 18:08:39 +0300

    add navlink for dialog,route exact

--- src/components/Dialogs/Header/Dialogs.jsx
+++ src/components/Dialogs/Header/Dialogs.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
-            <div className= {s.dialogItems}>
-                <div className={s.dialog}>Sergey</div>
-                <div className={s.dialog}>Den</div>
-                <div className={s.dialog}>Evgen</div>
-                <div className={s.dialog}>Dima</div>
-                <div className={s.dialog + ' ' + s.active}>Roma</div> 
+            <div className={s.dialogItems}>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/1'>Sergey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Den</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Evgen</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>Dima</NavLink>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/5'>Roma</NavLink>
+                </div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hello!</div>
