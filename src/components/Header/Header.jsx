c13a2f4ef745fa1674d7f280ba806a69b65800f4
nero <shackiy.s@yandex.ru>
2020-02-20 17:26:07 +0300

    cookie,add login(/auth/me)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
-    return <header className= {s.header}>
-        <img src='https://dynamic.brandcrowd.com/asset/logo/e39702a4-90f2-4790-afbd-d1d0209b495e/logo?v=4&text=Speech' />  
+const Header = (props) => {
+    return <header className={s.header}>
+        <img src='https://dynamic.brandcrowd.com/asset/logo/e39702a4-90f2-4790-afbd-d1d0209b495e/logo?v=4&text=Speech' />
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login :
+                <NavLink to='/login'>Login</NavLink>}
+        </div>
+
     </header>
 }
 
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Profile from './Profile';
 import * as axios from 'axios';
 import { connect } from 'react-redux';
-import { setUserProfile } from './../../redux/profile-reducer';
+import { setUserProfile } from '../../redux/profile-reducer';
 import { withRouter } from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
