9928e61f43237fd4e7b038b65341984a903777df
nero <shackiy.s@yandex.ru>
2020-02-20 11:45:40 +0300

    withRouter,props.match.params

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = (props) => {
       <div className='app-wrapper-content'>
         <Route path='/dialogs'
           render={() => <DialogsContainer store={props.store}/>} />
-        <Route path='/profile'
+        <Route path='/profile/:userId?'
           render={() => <ProfileContainer  store={props.store}/>} />
           <Route path='/users'
           render={() => <UsersContainer  store={props.store}/>} />
