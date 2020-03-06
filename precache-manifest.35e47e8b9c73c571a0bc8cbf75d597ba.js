2aa2906848b4c31ba96f348dc90957444017fe06
nero <shackiy.s@yandex.ru>
2020-03-06 15:33:56 +0300

    Updates

---
+++ precache-manifest.35e47e8b9c73c571a0bc8cbf75d597ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1b759c5f5694f03c5a5",
+    "url": "/social_network-react-js-/static/css/main.aee62f44.chunk.css"
+  },
+  {
+    "revision": "f1b759c5f5694f03c5a5",
+    "url": "/social_network-react-js-/static/js/main.0bc944b3.chunk.js"
+  },
+  {
+    "revision": "c10342a535ee7a8830ca",
+    "url": "/social_network-react-js-/static/js/runtime~main.3c51dd1b.js"
+  },
+  {
+    "revision": "4441dcf024973754f055",
+    "url": "/social_network-react-js-/static/js/2.e56302dc.chunk.js"
+  },
+  {
+    "revision": "18adf620842c1ff73127",
+    "url": "/social_network-react-js-/static/css/3.dcfc6271.chunk.css"
+  },
+  {
+    "revision": "18adf620842c1ff73127",
+    "url": "/social_network-react-js-/static/js/3.43f8663e.chunk.js"
+  },
+  {
+    "revision": "6db09181ed93f7e8f2dac478623e3629",
+    "url": "/social_network-react-js-/static/media/user.6db09181.png"
+  },
+  {
+    "revision": "65a777f4f363531d523710af219ea85c",
+    "url": "/social_network-react-js-/static/media/preloader.65a777f4.gif"
+  },
+  {
+    "revision": "4a2ab18b97f2beb0d2ee58394360d8ca",
+    "url": "/social_network-react-js-/static/media/profile.4a2ab18b.jpeg"
+  },
+  {
+    "revision": "2b7f2a098f941f1fdbe211065dc657b6",
+    "url": "/social_network-react-js-/index.html"
+  }
+];
